9af0d293ccc7abfd751e4b6557260adbb3801124
MartinLahoumh <martinlahoumh103102@gmail.com>
2023-08-07 13:18:54 -0400

    changed icons

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -4,26 +4,51 @@ import about_me from '../images/about-me.png';
 import experience from '../images/experience.png';
 import art_animations from '../images/art-animations.png';
 import '../styles/Home.css';
+/*
+<div className='all-content'>
+            <div className='options'>
+                <div className='icon'>
+                    <img className='icon-img'src={about_me} />
+                    <p className='section'>About Me.</p>
+                </div>
+                <div className='icon'>
+                    <img className='icon-img' src={experience} />
+                    <p className='section'>Experience.</p>
+                </div>
+                <div className='icon'>
+                    <img className='icon-img' src={experience} />
+                    <p className='section'>Projects.</p>
+                </div>
+                <div className='icon'>
+                    <img className='icon-img' src={art_animations} />
+                    <p className='section'>Art/Animations.</p>
+                </div>
+            </div>
+            <div className='welcome-message'>
+                <img className='welcome' src={welcome} />
+                <img className='monkey' src={monkey} />
+            </div>
+        </div>*/
 function Home() {
   return (
     <body>
         <div className='all-content'>
             <div className='options'>
-                <div className = 'icon'>
-                    <img className='icon-img' src={about_me} />
-                    <p className='section'>About Me</p>
+                <div className='icon'>
+                    <img className='icon-img'src={about_me} />
+                    <p className='section'>About Me.</p>
                 </div>
-                <div className = 'icon'>
+                <div className='icon'>
                     <img className='icon-img' src={experience} />
-                    <p className='section'>Experience</p>
+                    <p className='section'>Experience.</p>
                 </div>
-                <div className = 'icon'>
+                <div className='icon'>
                     <img className='icon-img' src={experience} />
-                    <p className='section'>Personal Projects</p>
+                    <p className='section'>Projects.</p>
                 </div>
-                <div className = 'icon'>
+                <div className='icon'>
                     <img className='icon-img' src={art_animations} />
-                    <p className='section'>Art/Animation</p>
+                    <p className='section'>Art/Animations.</p>
                 </div>
             </div>
             <div className='welcome-message'>
