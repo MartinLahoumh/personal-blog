74a67cb97ee8523caf0e15619334e600f15fd181
MartinLahoumh <martinlahoumh103102@gmail.com>
2023-08-09 15:03:36 -0400

    fonts

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -3,61 +3,41 @@ import monkey from '../images/Monkey.gif';
 import about_me from '../images/about-me.png';
 import experience from '../images/experience.png';
 import art_animations from '../images/art-animations.png';
+import cardboard from '../images/cardboard.jpg';
 import projects from '../images/projects.png';
 import '../styles/Home.css';
-/*
-<div className='all-content'>
-            <div className='options'>
-                <div className='icon'>
-                    <img className='icon-img'src={about_me} />
-                    <p className='section'>About Me.</p>
-                </div>
-                <div className='icon'>
-                    <img className='icon-img' src={experience} />
-                    <p className='section'>Experience.</p>
-                </div>
-                <div className='icon'>
-                    <img className='icon-img' src={experience} />
-                    <p className='section'>Projects.</p>
-                </div>
-                <div className='icon'>
-                    <img className='icon-img' src={art_animations} />
-                    <p className='section'>Art/Animations.</p>
-                </div>
-            </div>
-            <div className='welcome-message'>
-                <img className='welcome' src={welcome} />
-                <img className='monkey' src={monkey} />
-            </div>
-        </div>*/
+
 function Home() {
   return (
-    <body>
+    <>
+        <body>
+        
         <div className='all-content'>
-            <div className='options'>
-                <div className='icon'>
-                    <img className='icon-img'src={about_me} />
-                    <p className='section'>About Me.</p>
+                <div className='options'>
+                    <div className='icon'>
+                        <img className='icon-img'src={about_me} />
+                        <h2 className='section-name'>About Me.</h2>
+                    </div>
+                    <div className='icon'>
+                        <img className='icon-img' src={experience} />
+                        <h2 className='section-name'>Experience.</h2>
+                    </div>
+                    <div className='icon'>
+                        <img id = 'projects' src={projects} />
+                        <h2 className='section-name'>Projects.</h2>
+                    </div>
+                    <div className='icon'>
+                        <img className='icon-img' src={art_animations} />
+                        <h2 className='section-name'>Art/Animations.</h2>
+                    </div>
                 </div>
-                <div className='icon'>
-                    <img className='icon-img' src={experience} />
-                    <p className='section'>Experience.</p>
-                </div>
-                <div className='icon'>
-                    <img id = 'projects' src={projects} />
-                    <p className='section'>Projects.</p>
-                </div>
-                <div className='icon'>
-                    <img className='icon-img' src={art_animations} />
-                    <p className='section'>Art/Animations.</p>
+                <div className='welcome-message'>
+                    <img className='welcome' src={welcome} />
+                    <img className='monkey' src={monkey} />
                 </div>
             </div>
-            <div className='welcome-message'>
-                <img className='welcome' src={welcome} />
-                <img className='monkey' src={monkey} />
-            </div>
-        </div>
-    </body>
+        </body>
+    </>
   );
 }
 
